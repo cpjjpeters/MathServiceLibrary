9826861c081238a4736f7c1ed2b720a5f01966a0
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2018-12-11 21:08:37 +0100

    New MathServiceLibraryDemo

--- MathServiceLibrary/IMathService.cs
+++ MathServiceLibrary/IMathService.cs
@@ -9,13 +9,13 @@ namespace MathServiceLibrary
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface IMathService
     {
         [OperationContract]
-        string GetData(int value);
+        int Add(MyNumbers obj);
 
         [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
+        int Subtract(MyNumbers obj);
 
         // TODO: Add your service operations here
     }
@@ -23,23 +23,14 @@ namespace MathServiceLibrary
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
     // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "MathServiceLibrary.ContractType".
     [DataContract]
-    public class CompositeType
+    public class MyNumbers
     {
-        bool boolValue = true;
-        string stringValue = "Hello ";
+        
 
         [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
+        public int Number1 { get; set; }
 
         [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
+        public int Number2 { get; set; }
     }
 }
